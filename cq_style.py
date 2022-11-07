37da79444114cd377bca8d881aa635742d24ca8c
Jordan Poles <jpdev.noreply@gmail.com>
2022-11-07 17:06:24 -0500

    Much improved commenting + functionality

--- cq_style.py
+++ cq_style.py
@@ -39,13 +39,32 @@ class StylishPart:
         dictfilt = lambda x, y: dict([ (i,x[i]) for i in x if i not in set(y)])
         return cls(**dictfilt(asdict(instance), ["floor_h", "lip_h"]))
 
-    #Should not be edited
-    #If variables need to be calculate on class instantiation, then this should be done under self.calc_vars()
+    # DO NOT EDIT/OVERWRITE!
+    # IF you do, you should make sure to include: self.check_config(); self.calc_vars();
+    #
+    # If variables need to be checked for errors on class instantiation, use self.check_config()
+    # If variables need to be calculated on class instantiation, use self.calc_vars()
     def __post_init__(self):
         self.calc_vars()
+        self.check_config()
 
+    # An "optional" user-defined function which calculates class/self variables which are dependent on the values of params set during instatiation of the class
+    # Ex: you want to define a dimension as a fraction of a customizable dimension which is input by a user when instantiating the part
     def calc_vars(self):
-        return None
+        #Example: L is equal to 2x W
+        #self.L = 2 * self.W
+        return
+
+
+    # An "optional" user-defined function which checks over params from instantiation of the class
+    # Should be used to throw warnings/errors if variables are configured incorrectly
+    # Runs after calc_vars() on default init so can be used to check calculated variables too
+    # Ex: You want to make sure that one dimension is always smaller than the other for a customizable part...
+    def check_config(self):
+        #Example: L should always be greater than H
+        #if self.H > self.L:
+        #    raise ValueError("Invalid Config: self.H > self.L; height should always be less than length!)") 
+        return
 
     def make(self):
         #Replace with your own function!
@@ -55,21 +74,21 @@ class StylishPart:
         )
         return part
 
-    #Either retrives the cached version of a part, or regenerates it
+    # Either retrives the cached version of a part, or regenerates it
     def part(self, regen: bool = False):
         if self._stored_part == None or regen:
             self._stored_part = self.make()
         return self._stored_part
 
-    #Display the object using _show_object()
-    #Eg. if using CQ Editor: [show_object, debug, ...]
-    #Eg. if using Jupyter CadQuery: [show, ...]
+    # Display the object using _show_object()
+    # Ex: if using CQ Editor: [show_object, debug, ...]
+    # Ex: if using Jupyter CadQuery: [show, ...]
     def display(self, _show_object, regen: bool = False):
         _show_object(self.part(regen), name=self.part_name)
         return self
     
-    #Splits object in half along the provided axis (eg: ["XZ", "YZ", "XY"]) or Plane and then displays
-    def display_split(self, _show_object, regen: bool = False, axis="XZ"):
+    # Splits object in half along the provided axis (eg: ["XZ", "YZ", "XY"]) or Plane and then displays
+    def display_split(self, _show_object, regen: bool = False, axis="XZ", offset=0):
         p = self.part(regen)
         if isinstance(p, Assembly):
             #Allows for splitting of Assembly while maintaining colors
@@ -78,7 +97,7 @@ class StylishPart:
                 location = p.findLocation(name)
                 for shape in subpart.shapes:
                     cross_section.add(
-                        Workplane("XY").add(shape.located(location)).copyWorkplane(Workplane(axis)).split(0,1),
+                        Workplane("XY").add(shape.located(location)).copyWorkplane(Workplane(axis).workplane(offset=offset)).split(0,1),
                         color=subpart.color,
                         name=name,
                     )
@@ -87,7 +106,7 @@ class StylishPart:
             _show_object(p.copyWorkplane(Workplane(axis)).split(0,1), name=self.part_name)
         return self
 
-    #Exports model (shape or assembly) to file
+    # Exports model (shape or assembly) to file
     def export(self, filepath: str, regen: bool = False):
         p = self.part(regen)
         if isinstance(p, Assembly):
@@ -96,7 +115,7 @@ class StylishPart:
             exporters.export(p, filepath)
         return self
 
-    #Splits model (shape or assembly) in half along the provided axis (eg: ["XZ", "YZ", "XY"]) or Plane and then exports
+    # Splits model (shape or assembly) in half along the provided axis (eg: ["XZ", "YZ", "XY"]) or Plane and then exports
     def export_split(self, filepath: str, regen: bool = False, axis="XZ"):
         p = self.part(regen)
         if isinstance(p, Assembly):
--- example.py
+++ example.py
@@ -2,16 +2,49 @@ from cadquery import *
 from dataclasses import dataclass
 
 import sys
-sys.path.append("../cq_style")
+sys.path.append("../cq_style") #This should be set to the path of the `cq_style` directory absolute/relative to your project's working directory
 from cq_style import StylishPart
 
 @dataclass
 class NewPart(StylishPart):
-    part_param: float = 1234
+    # This allows the user to set two customizable dimensions, the length and the height
+    # We want the length to always be greater than the height and the width to equal 2x the length
+    part_W: float = 10
+    part_H: float = 8
+    wall_thickness: float = 1.5
+
+    # An "optional" user-defined function which checks over params from instantiation of the class
+    # Should be used to throw warnings/errors if variables are configured incorrectly
+    def check_config(self):
+        #Example: We want the length to always be greater than the height
+        if self.part_H > self.part_L:
+            raise ValueError("Invalid Config: self.H > self.L; height should always be less than length!)") 
+        return
+
+    # An "optional" user-defined function which calculates class/self variables which are dependent on the values of params set during instatiation of the class
+    # Ex: you want to define a dimension as a fraction of a customizable dimension which is input by a user when instantiating the part
+    def calc_vars(self):
+        #Example: We want the width to equal 2x the length
+        self.part_L = 2 * self.part_W
+        return
+
     def make(self):
-        part = Workplane("XY").sphere(1)
-        return part
+        
+        a = Assembly()
+        
+        part = (
+            Workplane("XY").box(self.part_L, self.part_W, self.part_H, centered=[1,1,0])
+            .faces(">Z").shell(-self.wall_thickness)
+            .faces("<Z").circle(self.part_W * 0.2).cutThruAll()
+        )
+
+        a.add(part)
+
+        return a
 
 if "show_object" in locals():
-    NewPart().display(show_object)
-    NewPart().display_split(show_object)
+    p1 = NewPart().display(show_object)
+    # Get creative and you can find many ways to display different aspects of your part
+    # The display_split() function will cut parts (including assemblies) in half along an axis
+    # to show features that might otherwise be difficult to visualize
+    NewPart().display_split(lambda p, name: show_object(p.translate(Vector(0, 0, p1.part_H * 1.5))), axis="YZ")
